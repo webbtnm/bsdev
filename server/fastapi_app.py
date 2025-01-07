038722fe65981fcc6679266df7dc09bd6fc9668b
Jange4ik PC <jangthe3@gmail.com>
2025-01-07 23:08:08 +0500

    +cors

--- server/fastapi_app.py
+++ server/fastapi_app.py
@@ -1,7 +1,20 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from server.fastapi_routes import router as routes_router
 
 app = FastAPI()
+
+origins = [
+    "http://localhost:5173"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 def root_route():
